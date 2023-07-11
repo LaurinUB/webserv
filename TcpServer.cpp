94f5eec34061d23f5b50a91ac0c7ceb32e1237f6
Philipp <51891575+philippschwarzmueller@users.noreply.github.com>
2023-07-11 14:09:44 +0200

    add working signalhandler for sigint and setting socket options for address reuse (#5)
    * add working signalhandler for sigint
    
    * fix style/formatting
    
    * improve variable naming
    
    * fixed signal handling
    
    * Update TcpServer.hpp
    
    * Update TcpServer.cpp
    
    * Update TcpServer.cpp
    
    * lint conform
    
    * i need formater?
    
    ---------
    
    Co-authored-by: LaurinUB <Laurinub@yahoo.de>
    Co-authored-by: Laurin <44403262+LaurinUB@users.noreply.github.com>

--- TcpServer.cpp
+++ TcpServer.cpp
@@ -1,8 +1,8 @@
 #include "TcpServer.hpp"
 
-#include <fstream>
-
 #include "HTTPResponse.hpp"
+
+extern sig_atomic_t g_signaled;
 
 const int BUFFER_SIZE = 30720;
 const int QUEUE_LEN = 20;
@@ -45,43 +45,49 @@ TcpServer& TcpServer::operator=(const TcpServer& obj) {
 }
 
 int TcpServer::startServer() {
-  socket_ = socket(AF_INET, SOCK_STREAM, 0);
-  if (socket_ < 0) {
+  int opt = 1;
+  this->socket_ = socket(AF_INET, SOCK_STREAM, 0);
+  if (this->socket_ < 0) {
     exitWithError("Cannot create socket");
     return EXIT_FAILURE;
   }
-  if (bind(socket_, reinterpret_cast<sockaddr*>(&socketAddress_),
-           socketAddress_len_) < 0) {
-    exitWithError("Cannot connect socket to address");
+  if (setsockopt(this->socket_, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) ==
+      -1) {
+    exitWithError("Cannot set socket options");
+  }
+  if (bind(this->socket_, (struct sockaddr*)&this->socketAddress_,
+           this->socketAddress_len_) < 0) {
+    exitWithError("Cannot bind to socket address");
     return EXIT_FAILURE;
   }
   return EXIT_SUCCESS;
 }
 
 void TcpServer::closeServer() const {
-  close(socket_);
-  close(new_socket_);
+  close(this->socket_);
+  close(this->new_socket_);
   exit(EXIT_SUCCESS);
 }
 
 void TcpServer::startListen() {
-  if (listen(socket_, QUEUE_LEN) < 0) {
+  if (listen(this->socket_, QUEUE_LEN) < 0) {
     exitWithError("Socket listen failed");
   }
 
   std::ostringstream ss;
-  ss << "\n*** Listening on ADDRESS: " << inet_ntoa(socketAddress_.sin_addr)
-     << " PORT: " << ntohs(socketAddress_.sin_port) << " ***\n\n";
+  ss << "\n*** Listening on ADDRESS: "
+     << inet_ntoa(this->socketAddress_.sin_addr)
+     << " PORT: " << ntohs(this->socketAddress_.sin_port) << " ***\n\n";
   log(ss.str());
 
   ssize_t bytesReceived = 0;
 
-  while (true) {
+  while (g_signaled == 0) {
     log("====== Waiting for a new connection ======\n\n\n");
-    acceptConnection(new_socket_);
+    acceptConnection(this->new_socket_);
 
     char buffer[BUFFER_SIZE] = {0};
-    bytesReceived = read(new_socket_, buffer, BUFFER_SIZE);
+    bytesReceived = read(this->new_socket_, buffer, BUFFER_SIZE);
     if (bytesReceived < 0) {
       exitWithError("Failed to read bytes from client socket connection");
     }
@@ -92,18 +98,18 @@ void TcpServer::startListen() {
 
     sendResponse();
 
-    close(new_socket_);
+    close(this->new_socket_);
   }
 }
 
 void TcpServer::acceptConnection(int& new_socket) {
-  new_socket = accept(socket_, reinterpret_cast<sockaddr*>(&socketAddress_),
-                      &socketAddress_len_);
+  new_socket = accept(this->socket_, (struct sockaddr*)&this->socketAddress_,
+                      &this->socketAddress_len_);
   if (new_socket < 0) {
     std::ostringstream ss;
     ss << "Server failed to accept incoming connection from ADDRESS: "
-       << inet_ntoa(socketAddress_.sin_addr)
-       << "; PORT: " << ntohs(socketAddress_.sin_port);
+       << inet_ntoa(this->socketAddress_.sin_addr)
+       << "; PORT: " << ntohs(this->socketAddress_.sin_port);
     exitWithError(ss.str());
   }
 }
@@ -120,9 +126,10 @@ std::string TcpServer::buildResponse() {
 void TcpServer::sendResponse() {
   ssize_t bytesSent = 0;
 
-  bytesSent = write(new_socket_, serverMessage_.c_str(), serverMessage_.size());
+  bytesSent = write(this->new_socket_, this->serverMessage_.c_str(),
+                    this->serverMessage_.size());
 
-  if (bytesSent == static_cast<ssize_t>(serverMessage_.size())) {
+  if (bytesSent == static_cast<ssize_t>(this->serverMessage_.size())) {
     log("------ Server Response sent to client ------\n\n");
   } else {
     log("Error sending response to client");
--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
 #include "TcpServer.hpp"
 
+sig_atomic_t g_signaled = 0;
+
+void handleSIGINT(int param) {
+  g_signaled = param;
+  _exit(SIGINT);
+}
+
 int main() {
-  TcpServer server = TcpServer("0.0.0.0", 6969);
+  signal(SIGINT, handleSIGINT);
+  TcpServer server = TcpServer("127.0.0.1", PORT);
   server.startListen();
   return 0;
 }
