264b68e0dcdb5c48d6545d7cfc398104c2d6b28f
Laurin <44403262+LaurinUB@users.noreply.github.com>
2023-09-07 09:12:52 +0200

    Http redir (#87)
    * redirection working
    
    * fix-formating

--- src/parser/LocationSettings.cpp
+++ src/parser/LocationSettings.cpp
@@ -8,18 +8,19 @@ LocationSettings::LocationSettings()
       allow_upload_(false),
       upload_dir_("./www/uploads") {
   allowed_methods_.push_back("GET");
-};
+}
 
-LocationSettings::~LocationSettings(){};
+LocationSettings::~LocationSettings() {}
 
 LocationSettings::LocationSettings(const LocationSettings& obj)
     : allowed_methods_(obj.allowed_methods_),
       root_(obj.root_),
       endpoint_(obj.endpoint_),
       default_(obj.default_),
+      redirect_(obj.redirect_),
       auto_index_(obj.auto_index_),
       allow_upload_(obj.allow_upload_),
-      upload_dir_(obj.upload_dir_){};
+      upload_dir_(obj.upload_dir_) {}
 
 LocationSettings& LocationSettings::operator=(const LocationSettings& obj) {
   this->allowed_methods_ = obj.allowed_methods_;
@@ -29,8 +30,9 @@ LocationSettings& LocationSettings::operator=(const LocationSettings& obj) {
   this->auto_index_ = obj.auto_index_;
   this->allow_upload_ = obj.allow_upload_;
   this->upload_dir_ = obj.upload_dir_;
+  this->redirect_ = obj.redirect_;
   return *this;
-};
+}
 
 bool LocationSettings::setValue(std::string key, std::string value) {
   value.erase(value.size() - 1);
@@ -48,26 +50,28 @@ bool LocationSettings::setValue(std::string key, std::string value) {
     this->allow_upload_ = value == "true" ? true : false;
   } else if (key == "upload-directory") {
     this->upload_dir_ = value;
+  } else if (key == "redirect") {
+    this->redirect_ = value;
   } else {
     return false;
   }
   return true;
-};
+}
 
 const std::vector<std::string>& LocationSettings::getAllowedMethods() const {
   return this->allowed_methods_;
-};
+}
 
-std::string LocationSettings::getRoot() const { return this->root_; };
+std::string LocationSettings::getRedir() const { return this->redirect_; }
+
+std::string LocationSettings::getRoot() const { return this->root_; }
 
 std::string LocationSettings::getEndpoint() const { return this->endpoint_; }
 
 std::string LocationSettings::getDefault() const { return this->default_; }
 
-bool LocationSettings::getAutoIndex() const { return this->auto_index_; };
+bool LocationSettings::getAutoIndex() const { return this->auto_index_; }
 
-bool LocationSettings::getAllowUpload() const { return this->allow_upload_; };
+bool LocationSettings::getAllowUpload() const { return this->allow_upload_; }
 
-std::string LocationSettings::getUploadDir() const {
-  return this->upload_dir_;
-};
+std::string LocationSettings::getUploadDir() const { return this->upload_dir_; }
